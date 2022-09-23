f4fb7e955152593d1064e818178a2932ba29b3bb
iammuqeet-ahmad <muqeet.ahmad@devsinc.com>
2022-09-23 18:08:01 +0500

    Deploying Project on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,23 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
 
+  # Full error reports are disabled and caching is turned on.
+  config.action_controller.perform_caching = true
+  config.consider_all_requests_local = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'muqeet.ahmad@devsinc.com',
+    password: 'uvkkwgdyymglxszk',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
