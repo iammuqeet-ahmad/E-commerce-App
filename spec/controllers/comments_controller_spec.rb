6c04655a0e836eed09636ceb3696f5ea7ad1a612
iammuqeet-ahmad <muqeet.ahmad@devsinc.com>
2022-10-14 23:45:12 +0500

    2 - Covered improvement point for Re-evaluation of test cases

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -13,61 +13,55 @@ RSpec.describe CommentsController, type: :controller do
   end
 
   describe 'Post #create' do
-    before do
-      @prevCommentCount = Comment.count
+    let(:prev_comment_count) {Comment.count}
+    let(:params) do
+      {
+        content:'This is awesome product'
+      }
     end
+
     context 'Successfully creating the comment' do
-      before do
-        post :create, xhr: true, params: { product_id: product.id, comment: { content: comment.content } }
-      end
 
-      it 'successful creation of comment checking status' do
+      it 'should successful create the comment checking through(http status,flash,count of comment)' do
+        post :create, xhr: true, params: { product_id: product.id, comment: params }
         expect(response).to have_http_status(:ok)
-      end
-
-      it 'successful creation of comment checking flash message' do
         expect(flash[:success]).to eq(I18n.t(:comment_created_successfully))
+        expect(Comment.all.count).to eq(prev_comment_count )
       end
 
-      it 'successful creation of comment checking comment count' do
-        expect(Comment.all.count).to eq(@prevCommentCount + 2) #####
-      end
     end
 
     context 'Comment creation unsuccessful' do
-      before do
-        post :create, xhr: true, params: { product_id: product.id, comment: { content: '' } }
-      end
-      it 'comment creation unsuccessful' do
+
+      it 'should not create comment checking through(flash,comment count)' do
+        params[:content] = ''
+        post :create, xhr: true, params: { product_id: product.id, comment: params }
         expect(flash[:error]).to eq(I18n.t(:comment_creation_unsuccessful))
+        expect(prev_comment_count).to eq(Comment.count)
       end
-      it 'comment creation unsuccessful checking if comment count changed' do
-        expect(@prevCommentCount).to eq(Comment.count)
-      end
+
     end
   end
 
   describe 'Get #edit' do
     context 'When user is signed in edit comment successfully' do
-      before do
+
+      it 'should successfully render the edit form checking through(render template,http status)' do
         get :edit, params: { product_id: product.id, id: comment.id }
-      end
-
-      it 'render the edit form successfully' do
         expect(response).to render_template :edit
-      end
-
-      it 'Successfully complete the http request status :ok' do
         expect(response).to have_http_status(:ok)
       end
+
     end
 
     context 'When user is signed out Edit unsuccessful' do
-      it 'Edition of the comment unsuccessful' do
+      
+      it 'should not render edit form checking through(flash)' do
         sign_in(user2)
         get :edit, params: { product_id: product.id, id: comment.id }
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
+
     end
   end
 
@@ -75,8 +69,10 @@ RSpec.describe CommentsController, type: :controller do
     before do
       @params = { product_id: product.id, id: comment.id, comment: { content: 'I dont like this product' } }
     end
-    context 'Comment updation Successful' do
-      it 'successfully update the comment' do
+
+    context 'Comment updation Successfull' do
+      
+      it 'should successfully update the comment' do
         put :update, xhr: true, params: @params
         expect(flash[:success]).to eq(I18n.t(:comment_updated_successfully))
       end
@@ -86,56 +82,58 @@ RSpec.describe CommentsController, type: :controller do
         comment_updation = Comment.find(comment.id)
         expect(comment_updation.content).to eq(@params[:comment][:content])
       end
+
     end
 
     context 'Updation of Comment unsuccessful' do
+
       before do
         @params[:comment][:content] = ''
       end
-      it 'updation of comment unsuccessful' do
+
+      it 'should not update the comment checking through(flash)' do
         sign_in(user2)
         put :update, params: @params
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
 
-      it 'updation of comment unsuccessful and render edit' do
+      it 'should not update the comment checking through(render template)' do
         put :update, params: @params
         expect(response).to render_template :edit
       end
+
     end
   end
 
   describe 'Delete #destroy' do
-    before do
-      @prev_comment_count = Comment.count
-      @params = { product_id: product.id, id: comment.id }
+
+    let(:prev_comment_count) {Comment.count}
+    let(:params) do
+      {
+        product_id: product.id,
+        id: comment.id
+      }
     end
+
     context 'Successfully deleting the comment' do
-      before do
-        delete :destroy, xhr: true, params: @params
-      end
-      it 'successfully deletes the comment' do
+
+      it 'should successfully deletes the comment checking through (flash,comment count)' do
+        delete :destroy, xhr: true, params: params
         expect(flash[:success]).to eq(I18n.t(:comment_deleted_successfully))
+        expect(Comment.count).to eq(prev_comment_count)
       end
 
-      it 'successfully deletes the comment and check comment count' do
-        expect(Comment.count).to eq(@prev_comment_count)##
-      end
     end
 
-    context 'Delettion of Comment Unsuccessful' do
-      it 'Deletion of comment unsuccessful' do
+    context 'Deletion of Comment Unsuccessful' do
+      
+      it 'should not deletes the comment checking through(flash)' do
         sign_in(user2)
-        delete :destroy, params: @params
+        delete :destroy, params: params
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
 
-      it 'Deletion of comment unsuccessful checking through count' do
-        @prev_comment_count2 = Comment.count
-        sign_in(user2)
-        delete :destroy, params: @params
-        expect(Comment.count).to eq(@prev_comment_count2)
-      end
     end
   end
+
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -13,268 +13,258 @@ describe ProductsController, type: :controller do
 
   describe 'Get #index' do
     context 'When user is signed in' do
-      before { get :index }
 
-      it 'Returns all the products excepts users products' do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'Renders index template' do
-        expect(response).to render_template :index
-      end
-
-      it 'Returns all the products excepts users products checking using count(products)' do
+      it 'should returns all the products excepts users products checking through (status,count,render)' do
+        get :index
         products = assigns(:products)
+        expect(response).to have_http_status(:ok)
+        expect(response).to render_template :index
         expect(products.count).to eq(Product.all.count - user.products.count)
       end
+
     end
 
     context 'When user is signed out' do
-      before do
+
+      it 'should return all the products avaible on store checking using count(products,http status)' do
         sign_out(user)
         get :index
-      end
-
-      it 'Returns all the products avaible on store' do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'Returns all the products avaible on store checking using count(products)' do
         products = assigns(:products)
+        expect(response).to have_http_status(:ok)
         expect(Product.all.count).to eq(products.count)
       end
+
     end
   end
 
   describe 'Get #my_products' do
     context 'When user is signed in' do
-      before do
-        get :my_products
-      end
 
-      it 'Returns all the products of the user' do
+      it 'should returns all the products of the user' do
+        get :my_products
+        products = assigns(:products)
+        expect(user.products.count).to eq(products.count)
         expect(response).to have_http_status(:ok)
       end
 
-      it 'Returns all the products of the user checking using count(user.products)' do
-        products = assigns(:products)
-        expect(user.products.count).to eq(products.count)
-      end
     end
 
     context 'When user is signed out' do
-      it 'Unsuccessful to get any product of user' do
+
+      it 'should not returns any product of user' do
         sign_out(user)
         get :my_products
         expect(flash[:alert]).to eq(I18n.t(:login_failed))
       end
+
     end
   end
 
   describe 'Get #new' do
     context 'When user is signed in' do
-      before do
+
+      it 'should instantiate instance variable of product checking through(http status,render template)' do
         get :new
-      end
-
-      it 'Instantiate instance variable' do
         expect(response).to have_http_status(:ok)
-      end
-
-      it 'Renders new template' do
         expect(response).to render_template :new
       end
+
     end
 
     context 'When user is signed out' do
-      it 'New instance variable will not be initialized' do
+
+      it 'should not initialize new instance variable checking through(flash,redirect path)' do
         sign_out(user)
         get :new
         expect(flash[:alert]).to eq(I18n.t(:login_failed))
         expect(response).to redirect_to new_user_session_path
       end
+
     end
   end
 
   describe 'Post #create' do
-    context 'Product creation Successful' do
-      it 'Create product successfully' do
-        post :create,
-             params: { product: product.attributes }
-        expect(flash[:success]).to eq(I18n.t(:product_successfully_created))
+    let(:prev_product_count) {Product.count}
+
+    context 'Successfully creating the product' do
+      let(:params) do
+        {
+          product: 
+          {
+            name: "Nike Air Shoes",
+            description: "Brand new shoes",
+            price: 4000,
+            serialNo: "123ab"
+          }
+        }
       end
+
+      it 'should create product successfully and also check the count of the product' do
+        post :create, params: params
+        expect(flash[:success]).to eq(I18n.t(:product_successfully_created))
+        expect(Product.all.count).to eq(prev_product_count)
+      end
+
     end
 
     context 'Product creation Unsuccessful' do
-      it 'Product creation failed because of name nill' do
-        product.name = ''
-        post :create,
-             params: { product: product.attributes }
+
+      let(:params) do
+        {
+          product: 
+          {
+            name: "",
+            description: "Brand new shoes",
+            price: 4000
+          }
+        }
+      end
+
+      it 'should not create product checking through (render template,count)' do 
+        post :create, params: params 
+        expect(Product.all.count).to eq(prev_product_count)
         expect(response).to render_template :new
       end
 
-      it 'Product creation failed beacause of description nill' do
-        product.description = ''
-        post :create,
-             params: { product: product.attributes }
-        expect(response).to render_template :new
-      end
     end
   end
 
   describe 'Get #show' do
+    
     context 'Show the product' do
-      before do
+
+      it 'should show all the details of the product checking through (http status, product exists)' do
         get :show, params: { id: product.id }
-      end
-
-      it 'Shows the details of the product' do
         expect(response).to have_http_status(:ok)
-      end
-
-      it 'Shows the details of the product checking (if product exists returns true)' do
         expect(assigns(:product)).to be_truthy
       end
+
     end
 
     context 'Show the product Fails' do
-      before do
+
+      it 'should not shows details of the product checking through (http status, product exists)' do
         get :show, params: { id: -1 }
-      end
-
-      it 'Shows the product details fails' do
         expect(response).to have_http_status(:not_found)
-      end
-
-      it 'Shows the product fails (if product exists returns false)' do
         expect(assigns(:product)).to be_falsey
       end
+
     end
   end
 
   describe 'Get #edit' do
     context 'When user is signed in edit successfully' do
-      before do
+
+      it 'should render the edit form successfully checking through(render template,http status)' do
         get :edit, params: { id: product.id }
-      end
-
-      it 'Render the edit form successfully' do
         expect(response).to render_template :edit
-      end
-
-      it 'Successfully complete the http request status :ok' do
         expect(response).to have_http_status(:ok)
       end
+
     end
 
     context 'When user is signed out edit unsuccessful' do
-      it 'Edition of the product unsuccessful' do
+
+      it 'should not render edit form of the product checking through(flash)' do
         sign_in(user2)
         get :edit, params: { id: product.id }
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
+
     end
   end
 
   describe 'Put #update' do
-    before do
-      @params = { name: 'pants', description: 'These are cotton pants', price: 5000 }
+
+    let(:params) do
+      {
+        name: "pants",
+        description: "These are cotton pants",
+        price: 6000
+      }
     end
+
     context 'Successfully updating the product' do
-      it 'Successfully update the product' do
-        put :update, params: { id: product.id, product: @params }
+
+      it 'should successfully update the product checking through(flash,{price,quantity,description updation})' do
+        put :update, params: {id: product.id, product: params }
+        product_updation = assigns(:product)
         expect(flash[:success]).to eq(I18n.t(:product_successfully_updated))
+        expect(product_updation.name).to eq(params[:name])
+        expect(product_updation.description).to eq(params[:description])
+        expect(product_updation.price).to eq(params[:price])
       end
 
-      it 'Successfully updating the product (checking updation)' do
-        put :update, params: { id: product.id, product: @params }
-        product_updation = assigns(:product)
-        expect(product_updation.name).to eq(@params[:name])
-        expect(product_updation.description).to eq(@params[:description])
-        expect(product_updation.price).to eq(@params[:price])
-      end
     end
 
     context 'Updation of product Unsuccessful' do
-      it 'Updation of Product unsuccessful' do
+
+      it 'should not update the Product checking through user not signed in' do
         sign_in(user2)
-        put :update, params: { id: product.id, product: @params }
+        put :update, params: { id: product.id,product: params }
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
 
-      it 'Updation of Product Unsuccessful and render edit' do
-        @params[:name] = ''
-        put :update, params: { id: product.id, product: @params }
+      it 'should not update the product checking through render template' do
+        params[:name] = ''
+        put :update, params: { id: product.id,product: params }
         expect(response).to render_template :edit
       end
+
     end
   end
 
   describe 'delete #destroy' do
     context 'Successfully deleting the product' do
-      before do
-        @countProducts = Product.all.count
+      
+      it 'should successfully deletes the product' do
+        countProducts = Product.count
         delete :destroy, params: { id: product.id }
-      end
-      it 'Successfully deletes the product' do
         expect(flash[:success]).to eq(I18n.t(:product_successfully_deleted))
-      end
-
-      it 'Successfully deletes the product and redirect' do
         expect(response).to redirect_to products_path
+        expect(Product.count).to eq(countProducts)
       end
 
-      it 'Successfully deleting the product and checking count decrement' do
-        expect(Product.all.count).to eq(@countProducts)
-      end
     end
 
     context 'Deletion of the product Failed ' do
-      it 'Deletion of product Unsuccessful because of wrong id' do
+      
+      it 'should not deletes the product because of wrong id' do
         delete :destroy, params: { id: -1 }
         expect(response).to have_http_status(:not_found)
       end
 
-      it 'Deletion of product Unsuccessful because of unauthorized user' do
+      it 'should not deletes the product because of unauthorized user' do
         sign_in(user2)
         delete :destroy, params: { id: product.id }
         expect(flash[:alert]).to eq(I18n.t(:not_authorized))
       end
+
     end
   end
 
   describe 'update quantity of product' do
     context 'Successfully updating the quantity of the product' do
-      before do
+
+      it 'should successfuly updates the quantity of the product checking through (flash,redirect path,quantity count increased)' do
         patch :update_quantity, params: { id: product.id, quantity: 7 }
-      end
-
-      it 'Successfuly updates the quantity of the product' do
         expect(flash[:notice]).to eq(I18n.t(:quantity_successfully_updated))
-      end
-
-      it 'Successfuly updates the quantity of the product and redirect back to cart' do
         expect(response).to redirect_to carts_path
-      end
-
-      it 'Successfuly updates the quantity of the product checking quanity count' do
         product2 = Product.find(product.id)
         expect(product2.quantity).to eq(7)
       end
+
     end
 
     context 'Updation of quanity of the product is Unsuccessful' do
-      before do
-        patch :update_quantity, params: { id: product.id, quantity: -1 }
-      end
-      it 'Quantity updation Unsuccessful' do
-        expect(flash[:alert]).to eq(I18n.t(:quantity_updation_unsuccessfully))
-      end
 
-      it 'Quantity updation Unsuccessful checking quanity count' do
+      it 'should not update the quantity of the product checking through (flash,quantity count)' do
+        patch :update_quantity, params: { id: product.id, quantity: -1 }
+        expect(flash[:alert]).to eq(I18n.t(:quantity_updation_unsuccessfully))
         product2 = Product.find(product.id)
         expect(product2.quantity).to eq(1)
       end
+
     end
   end
 end
